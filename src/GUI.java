7cd1e68e8b97b8385ec363ab36d4e0ea6e10a131
Marcus Billman <xfoxx37@gmail.com>
2020-03-12 11:06:03 +0100

    Make initial form

--- src/GUI.java
+++ src/GUI.java
@@ -1,10 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * GUI
  * 2020-03-12
  * Marcus Billman
  */
-public class GUI {
-    public static void main(String[] args) {
 
+public class GUI {
+    private JPanel panel;
+    private JTextArea textArea;
+
+    private static Dimension preferredSize = new Dimension(500, 400);
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+
+        JFrame frame = new JFrame("GUI");
+        frame.setTitle("NoTE18pad");
+        frame.setPreferredSize(preferredSize);
+        frame.setContentPane(new GUI().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
